2b8620f008834305c37102ba2a5a9cf4a7ccb785
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-10-30 23:45:43 +0100

    Update web/index.php

--- web/index.php
+++ web/index.php
@@ -2,9 +2,6 @@
          <?php
  echo "Version 1";
  echo exec('whoami');
- exec('python /home/pi/raspberry/web/test/temp_control.py',$output,$retval);
-echo( $output[0] ." is output and ".$retval. " is return");
-
-
+ echo exec('python /home/pi/raspberry/web/test/temp_control.py');
 		
          ?>
