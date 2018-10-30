4d75d8ceff684fef306a821c41574ab1b50eec96
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-10-30 23:05:13 +0100

    Update web/index.php

--- web/index.php
+++ web/index.php
@@ -1,24 +1,8 @@
-<html>
-<head>
- <meta name="viewport" content="width=device-width" />
- <title>LED Control</title>
- </head>
-         <body>
-         Torsteuerung wadu hek
-         <form method="get" action="index.php">
-                 <input type="submit" value="ON" name="on">
 
-         </form>
-		 
          <?php
  
  exec('python /home/pi/raspberry/temp_control.py',$output,$retval);
-        echo "<prev>";
-        var_dump($output);
-        print_r("My Farm");
-        print_r(" out  ".$output[0]);
-        print_r(" ret  ".$retval[0]);
+echo( $output[0] ." is output and ".$retval. " is return");
+
 		
          ?>
-         </body>
- </html>
