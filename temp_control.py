5a96f9d31cce04018854cfde19ef3f3fdc749e74
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-10-30 22:57:21 +0100

    Update temp_control.py

--- temp_control.py
+++ temp_control.py
@@ -1,10 +1,11 @@
 import sys
 import Adafruit_DHT
-require_once "/home/pi/raspberry/web/index.php"
+
 
 sensor = Adafruit_DHT.DHT11
 pin = 4
 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
-print temperature
+return temperature
+
 
