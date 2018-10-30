893b457166968826424f8ea3daad5083f1891fc9
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-10-30 22:55:02 +0100

    Update web/index.php

--- web/index.php
+++ web/index.php
@@ -16,8 +16,8 @@
         echo "<prev>";
         var_dump($output);
         print_r("My Farm");
-        print_r(" out  ".$output);
-        print_r(" ret  ".$retval);
+        print_r(" out  ".$output[0]);
+        print_r(" ret  ".$retval[0]);
 		
          ?>
          </body>
