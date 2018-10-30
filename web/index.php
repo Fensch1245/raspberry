c376bea0158d370c1315d475ddf9c1667c4398c1
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-10-30 23:54:33 +0100

    Update web/index.php

--- web/index.php
+++ web/index.php
@@ -1,7 +1,8 @@
 
          <?php
- echo "Version 1";
+ echo "Version 1 \n";
  echo exec('whoami');
+ echo "\n";
  echo exec('python /home/pi/raspberry/temp_control.py');
  echo exec('ls -l /home/pi/raspberry/temp_control.py');		
          ?>
