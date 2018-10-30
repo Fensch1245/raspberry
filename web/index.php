d18656146759b0c4412249581ca291bd759afe55
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-10-30 23:44:28 +0100

    Update web/index.php

--- web/index.php
+++ web/index.php
@@ -1,13 +1,10 @@
 
          <?php
  echo "Version 1";
- exec('whoami');
+ echo exec('whoami');
  exec('python /home/pi/raspberry/web/test/temp_control.py',$output,$retval);
 echo( $output[0] ." is output and ".$retval. " is return");
 
-foreach ($output as $item) {
-	echo "item";
-    echo $item;
-}
+
 		
          ?>
