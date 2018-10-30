8d750121b88b3a9bd0610940c5cd3c3e71fff41d
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-10-31 00:04:18 +0100

    Update web/index.php

--- web/index.php
+++ web/index.php
@@ -8,4 +8,9 @@
  echo "<br/>";
  echo exec('ls -l /home/pi/raspberry/temp_control.py');	
 echo "<br/>"; 
+
+
+    echo "Current user is: " . get_current_user();
+    echo "Command output is " . shell_exec("sudo python /home/pi/raspberry/temp_control.py");
+
          ?>
