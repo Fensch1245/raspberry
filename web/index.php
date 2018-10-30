6bec80915b1397d1a18276844ec6f8a442e0a327
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-10-30 23:32:42 +0100

    Update web/index.php

--- web/index.php
+++ web/index.php
@@ -1,7 +1,7 @@
 
          <?php
  echo "Version 1";
- exec('python /home/pi/raspberry/temp_control.py',$output,$retval);
+ exec('python /home/pi/raspberry/web/test/temp_control.py',$output,$retval);
 echo( $output[0] ." is output and ".$retval. " is return");
 
 foreach ($output as $item) {
