a2acc90d3e41cb2ad6fadbc73122d4f4ca8ac67b
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-10-30 22:50:00 +0100

    Update web/index.php

--- web/index.php
+++ web/index.php
@@ -9,13 +9,16 @@
                  <input type="submit" value="ON" name="on">
 
          </form>
+		 
          <?php
-
-   
-$out = shell_exec("/home/pi/raspberry/temp_control.py");
-echo "temp:";
-echo $out;        
-
+ 
+ exec('python /home/pi/raspberry/temp_control.py',$output,$retval);
+        echo "<prev>";
+        var_dump($output);
+        print_r("My Farm");
+        print_r(" out  ".$output);
+        print_r(" ret  ".$retval);
+		
          ?>
          </body>
  </html>
