7868a20d921aa42a97676be654be30865d03aec9
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-10-31 18:36:43 +0100

    Update web/index.php

--- web/index.php
+++ web/index.php
@@ -3,15 +3,22 @@
 <body>
 
 
-Name: <input type="text" name="name" id="name" ><br/>
-
-<input type="submit" name="submit" value="Submit">  
+<form action="index.php" method="POST">
+<input type="text" name="input_value">
+<input type="submit" name="submit">
 
 
 </body>
 </html>
 
          <?php
+		 
+		 if (isset($_POST['submit']))
+  {
+  // Execute this code if the submit button is pressed.
+  $formvalue = $_POST['input_value'];
+  }
+  
 echo "Aktuell gemessene Temperatur:";
 echo "<br/>"; 
 echo exec('sudo python /home/pi/raspberry/get_info.py');
