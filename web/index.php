6c8414f49ddb9086962da7d6101ed07722f2527e
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-10-31 17:39:04 +0100

    Update web/index.php

--- web/index.php
+++ web/index.php
@@ -2,7 +2,7 @@
          <?php
 echo "Aktuell gemessene Temperatur:";
 echo "<br/>"; 
-echo exec('sudo python /home/pi/raspberry/get_temp.py');
+echo exec('sudo python /home/pi/raspberry/get_info.py');
 echo "<br/>"; 
 
          ?>
