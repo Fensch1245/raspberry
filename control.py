efb694314e9f0956c5669a30c72c12dfc66791ca
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-10-31 20:17:08 +0100

    Update control.py

--- control.py
+++ control.py
@@ -6,9 +6,6 @@ import ch_config
 
 config = ConfigParser.ConfigParser()
 config.read('/home/pi/raspberry/config.ini')
-
-print(config.get('DEFAULT', 'set_temp'))
-
 ch_config.update_val('DEFAULT', 'set_temp', '25')
 
 
