89d6dc6867e8f731453b5253574038d3f14e7297
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-10-31 20:46:19 +0100

    Update web/index.php

--- web/index.php
+++ web/index.php
@@ -13,9 +13,7 @@ echo exec('sudo python /home/pi/raspberry/get_info.py');
 echo "<br/>"; 
 $test = $_POST['idtest'];
 echo $test;
-exec('sudo python /home/pi/raspberry/ch_config.py DEFAULT set_temp 30');
-
-echo exec('sudo python /home/pi/raspberry/ch_config.py DEFAULT set_temp $test');
+exec('sudo python /home/pi/raspberry/ch_config.py DEFAULT set_temp $test');
 
 
          ?>
