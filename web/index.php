9d5f15353d05f44fcbefcaabc656968ee24217c0
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-10-31 19:37:24 +0100

    Update web/index.php

--- web/index.php
+++ web/index.php
@@ -6,7 +6,11 @@ echo "Aktuell gemessene Temperatur:";
 echo "<br/>"; 
 echo exec('sudo python /home/pi/raspberry/get_info.py');
 echo "<br/>"; 
-echo shell_exec('sudo python /home/pi/raspberry/get_config.py');
+
+$result = shell_exec('sudo python /home/pi/raspberry/get_config.py');
+$results_array = json_decode($result);
+echo "<p>".$results_array."</p>";
+
 echo "<br/>"; 
 echo exec('ls -l /home/pi/raspberry/get_info.py');
 echo "<br/>"; 
