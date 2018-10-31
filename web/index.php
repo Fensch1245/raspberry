a0c5b5e26cd3cd1b4111dbd8fcedbf730f220aaa
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-10-31 19:00:45 +0100

    Update web/index.php

--- web/index.php
+++ web/index.php
@@ -1,16 +1,4 @@
-<html>
-<head>
- <meta name="viewport" content="width=device-width" />
- <title>Temp-Control</title>
- </head>
-         <body>
 
-         <form method="get" action="index.php">
-			<input type="text" name="input_value" value="<?php echo($wadu); ?>">
-            <input type="submit" name=input_value value="Speichern">
-
-         </form>
-  
 		 
 		 <?php
 		   
@@ -18,9 +6,7 @@ echo "Aktuell gemessene Temperatur:";
 echo "<br/>"; 
 echo exec('sudo python /home/pi/raspberry/get_info.py');
 echo "<br/>"; 
-
 echo exec('sudo python /home/pi/raspberry/get_config.py');
 
-echo $wadu;
+
          ?>
-</html>
