d35f43322407f1d07462e6fb140544a8cabb12ea
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-10-31 20:26:14 +0100

    Update ch_config.py

--- ch_config.py
+++ ch_config.py
@@ -2,9 +2,9 @@ import sys
 import ConfigParser
 
 
-def update_val(head, line, new_val):
+def update_val(sys.argv[1], sys.argv[2], sys.argv[3]):
 	config = ConfigParser.ConfigParser()
 	config.read('/home/pi/raspberry/config.ini')
-	config.set(head, line, new_val)
+	config.set(sys.argv[1], sys.argv[2], sys.argv[3])
 	with open("/home/pi/raspberry/config.ini", "w+") as configfile:
 		config.write(configfile)
