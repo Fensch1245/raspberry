9b21966cca0cba00af57edeed53b171c6290ab5e
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-10-31 20:40:23 +0100

    Update web/index.php

--- web/index.php
+++ web/index.php
@@ -14,10 +14,9 @@ echo "<br/>";
 $test = $_POST['idtest'];
 echo $test;
 
-
 if (isset($_POST['update']))
 {
-    echo exec('python ch_config.py DEFAULT set_temp 420');
+    echo exec('python ch_config.py DEFAULT set_temp '".$test."'");
 }
 
          ?>
