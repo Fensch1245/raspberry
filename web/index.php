e3f4438824802ad466dcec1f45c81861387eb5f2
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-10-31 19:40:51 +0100

    Update web/index.php

--- web/index.php
+++ web/index.php
@@ -7,8 +7,9 @@ echo "<br/>";
 echo exec('sudo python /home/pi/raspberry/get_info.py');
 echo "<br/>"; 
 
-$result = shell_exec('sudo python /home/pi/raspberry/get_config.py');
-echo "<pre>"$result"</pre>";
+$output = shell_exec('ls -lart');
+echo "<pre>$output</pre>";
+
 
 echo "<br/>"; 
 echo exec('ls -l /home/pi/raspberry/get_info.py');
