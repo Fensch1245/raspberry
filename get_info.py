184aee0bdea702a6c7e0569b1445d0cc020bbb91
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-10-31 17:34:56 +0100

    weil ichs kann

--- get_info.py
+++ get_info.py
@@ -10,6 +10,11 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 def get_temp():
 	return temperature
 
+def main(argv):
+    print(get_value(*argv[1:]))
 
+if __name__ == "__main__":
+    import sys
+    main(sys.argv)
 
 
