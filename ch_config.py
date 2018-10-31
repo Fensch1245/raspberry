44f83bd6e27064e552c988698f9fd048526491cd
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-10-31 20:27:21 +0100

    Update ch_config.py

--- ch_config.py
+++ ch_config.py
@@ -1,6 +1,9 @@
 import sys
 import ConfigParser
 
+print sys.argv[1] # prints var1
+print sys.argv[2] # prints var2
+print sys.argv[3] # prints var2
 
 def update_val(sys.argv[1], sys.argv[2], sys.argv[3]):
 	config = ConfigParser.ConfigParser()
