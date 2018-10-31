a20aa90be73e00d8187d5e09bbf9c12441667503
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-10-31 19:25:26 +0100

    Update get_config.py

--- get_config.py
+++ get_config.py
@@ -2,12 +2,12 @@ import sys
 import ConfigParser
 
 print "wadu1"
-
+print "wadu2"
 config = ConfigParser.ConfigParser()
 config.read('config.ini')
 result = config.get('DEFAULT', 'set_temp')
 
-print "wadu2"
+
 
 def set_temp():
 	return result
