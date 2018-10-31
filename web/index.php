14dc936069729edb1a5e1c322a622fcbf3c1f7a0
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-10-31 18:31:12 +0100

    Update web/index.php

--- web/index.php
+++ web/index.php
@@ -1,8 +1,22 @@
 
+<html>
+<body>
+
+<form name='form' method='post' action="index.php">
+
+Name: <input type="text" name="name" id="name" ><br/>
+
+<input type="submit" name="submit" value="Submit">  
+
+</form>
+</body>
+</html>
+
          <?php
 echo "Aktuell gemessene Temperatur:";
 echo "<br/>"; 
 echo exec('sudo python /home/pi/raspberry/get_info.py');
 echo "<br/>"; 
+
 
          ?>
