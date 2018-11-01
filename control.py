c4f55c96e8b9bec0e67781308357599ccd1c36fc
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-11-01 18:36:35 +0100

    Update control.py

--- control.py
+++ control.py
@@ -2,8 +2,32 @@ import sys
 import get_info
 import ConfigParser
 import os
+from lxml import html
+import requests
 
-os.system("sudo python /home/pi/raspberry/ch_config.py DEFAULT set_temp 69")
+page = requests.get('http://econpy.pythonanywhere.com/ex/001.html')
+tree = html.fromstring(page.content)
+
+#This will create a list of buyers:
+buyers = tree.xpath('//div[@title="buyer-name"]/text()')
+#This will create a list of prices
+prices = tree.xpath('//span[@class="item-price"]/text()')
+
+print 'Buyers: ', buyers
+print 'Prices: ', prices
+
+
+
+
+
+
+
+
+
+
+
+
+# os.system("sudo python /home/pi/raspberry/ch_config.py DEFAULT set_temp 69")
 
 
 
