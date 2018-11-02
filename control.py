49ef634942fbf9b74ad43302a9de03b129585dec
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-11-02 22:01:05 +0100

    Update control.py

--- control.py
+++ control.py
@@ -11,7 +11,7 @@ tree = html.fromstring(page.content)
 
 
 #This will create a list of buyers:
-buyers = tree.xpath('//*[@id="l1"]/table[2]/tbody/tr/td/div/text()')
+buyers = tree.xpath('//*[@id="l1"]/table[2]/tbody/')
 #This will create a list of prices
 prices = tree.xpath('//span[@class="item-price"]/text()')
 
