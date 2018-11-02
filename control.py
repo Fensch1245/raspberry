108a6c5453987d9d3b3a940c21604338da13863c
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-11-02 23:28:22 +0100

    Update control.py

--- control.py
+++ control.py
@@ -11,18 +11,25 @@ tree = html.fromstring(page.content)
 
 
 #This will create a list of buyers:
-buyers = tree.xpath('//*[@id="l1"]/text()')
+# buyers = tree.xpath('//*[@id="l1"]/text()')
 
 
 
-#This will create a list of prices
-prices = tree.xpath('//span[@class="item-price"]/text()')
+# #This will create a list of prices
+# prices = tree.xpath('//span[@class="item-price"]/text()')
 
-print 'Buyers: ', buyers
-print 'Prices: ', prices
+# print 'Buyers: ', buyers
+# print 'Prices: ', prices
 
 
+from selenium import webdriver
 
+browser = webdriver.Firefox()
+browser.get('http://192.168.2.102/')
+list_of_elements = ["a", "button", "li", "nav", "ol", "span", "ul", "header", "footer", "section"]
+for tag_name in list_of_elements:
+    for element in browser.find_elements_by_tag_name(tag_name):
+         print element
 
 
 
