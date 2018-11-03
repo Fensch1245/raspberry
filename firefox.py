9a01eca10aaa7db3368733caa952d5f9aacfd935
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-11-03 17:33:56 +0100

    Update firefox.py

--- firefox.py
+++ firefox.py
@@ -32,7 +32,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
-binary = FirefoxBinary('/usr/lib/firefox-esr/')
+binary = FirefoxBinary('/usr/lib/firefox-esr/firefox-bin')
 
 
 cap = DesiredCapabilities().FIREFOX
