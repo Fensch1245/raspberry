1b8c3e3f138bcd0c48c7f6deae2f7a9b5a1b3e27
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-11-03 14:25:17 +0100

    1

---
+++ chrome.py
@@ -0,0 +1,48 @@
+import sys
+import get_info
+import ConfigParser
+import os
+# from lxml import html
+# import requests
+
+# page = requests.get('http://192.168.2.102/')
+# tree = html.fromstring(page.content)
+
+
+# print(tree.text_content())
+# #This will create a list of buyers:
+# buyers = tree.xpath('//*[@id="l1"]')
+
+
+
+# #This will create a list of prices
+# prices = tree.xpath('//span[@class="item-price"]/text()')
+
+# print 'Buyers: ', buyers
+# print 'Prices: ', prices
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome(executable_path="/usr/local/bin/chromedriver")
+driver.get("https://tools.wmflabs.org/pageviews/?project=en.wikipedia.org&platform=all-access&agent=user&range=latest-20&pages=Star_Wars:_The_Last_Jedi")
+# element = driver.find_element_by_class_name("pull-right")
+# or the following below 
+# element = driver.find_element_by_name("q")
+# element = driver.find_element_by_id("html ID name")
+# element = driver.find_element_by_name("html element name")
+element = driver.find_element_by_xpath("//input[@id='passwd-id']")
+print(element)
+driver.close()
+
+
+
+
+
+
+
+
+# os.system("sudo python /home/pi/raspberry/ch_config.py DEFAULT set_temp 69")
+
+
+
