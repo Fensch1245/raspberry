9c38df6525ba81f2fb6b1332a070a1a8f37e0c29
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-11-03 02:20:53 +0100

    Update control.py

--- control.py
+++ control.py
@@ -24,7 +24,7 @@ import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+driver = webdriver.Chrome(executable_path="/usr/local/bin/chromedriver")
 driver.get("https://tools.wmflabs.org/pageviews/?project=en.wikipedia.org&platform=all-access&agent=user&range=latest-20&pages=Star_Wars:_The_Last_Jedi")
 # element = driver.find_element_by_class_name("pull-right")
 # or the following below 
