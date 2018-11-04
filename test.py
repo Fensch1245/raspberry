f984054a3f1172b839cfda0b2de0def0038ba1b5
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-11-04 19:43:38 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,15 +2,25 @@ import sys
 import get_info
 import ConfigParser
 import os
+from lxml import html
+import requests
+
+page = requests.get('http://192.168.2.102/ay')
+tree = html.fromstring(page.content)
+
+
+print(tree.text())
+
+
 #import requests
 
 #r = requests.get('http://192.168.2.102/ay')
 #print(r.text)
  
-import urllib3
-http = urllib3.PoolManager()
-r = http.request('GET', 'http://192.168.2.102/')
-r.status
-print(r.status)
-r.data
-print(r.data)
+# import urllib3
+# http = urllib3.PoolManager()
+# r = http.request('GET', 'http://192.168.2.102/ay')
+# r.status
+# print(r.status)
+# r.data
+# print(r.data)
