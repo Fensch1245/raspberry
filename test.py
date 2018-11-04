c7c80b77ccc61c2ce9deff6e2f14b18a00ac0766
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-11-04 21:02:51 +0100

    Update test.py

--- test.py
+++ test.py
@@ -19,7 +19,11 @@ import os
  
 import urllib3
 http = urllib3.PoolManager()
-r = http.request('GET', 'http://192.168.2.102/ay')
+r = http.request('GET', 'http://192.168.2.102/cm?cmnd=Power')
 
 with open('page_source.html', 'w') as fid:
     print(r.data)
+	
+	#http://192.168.2.102/cm?cmnd=status
+	
+	#http://192.168.2.102/cm?cmnd=Power
