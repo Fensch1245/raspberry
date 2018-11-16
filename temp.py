fdd543c6652ad0a28e1d61bcce7a4caa33f39f51
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-11-16 18:00:25 +0100

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,13 +1,4 @@
 # -*- coding: utf-8 -*-
-# from si7021 import Si7021
-# from time import sleep
-# from smbus import SMBus
-# sensor = Si7021(SMBus(1))
-# print("%.1f %%RH, %.1f °C" % sensor.read())
-# sensor.heater_mA = 50
-# sleep(10)
-# print("%.1f %%RH, %.1f °C" % sensor.read())
-# sensor.heater_mA = 0
 
 import smbus
 import time
