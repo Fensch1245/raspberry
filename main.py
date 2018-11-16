bf175534d7602186dc79bf5a06ecebff108fa2eb
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-11-16 18:45:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import get_temp
-
 import sys
+import time
 
-val = get_temp.ret_val()
 
-print val
+
+
+count = 0
+while count < 5:
+	val = get_temp.ret_val()
+    print(val)
+	time.sleep(0.5)
