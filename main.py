512df44ae3e3ced09f594c647220bcdb94d1ca5a
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-11-16 18:54:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,11 @@ import get_temp
 import sys
 import time
 
-val = get_temp.ret_val()
-print val
 
 
-count = 1
+count = 0
 while count < 1:
 	val = get_temp.ret_val()
 	print val
+	val.flush()
 	time.sleep(2)
