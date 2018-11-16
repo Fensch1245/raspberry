e6a2cf136a03f7a4d7c3a9e9b9cc9450ad4f7246
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-11-16 19:21:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import subprocess
 import sys
 import time
 
+
 count = 0
 while count < 1:
 	returned_bin =  subprocess.check_output('/home/pi/raspberry/get_temp.py'.split())
-	print returned_bin
 	time.sleep(2)
