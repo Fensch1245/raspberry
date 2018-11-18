08325a90fd90e3f6eea9517047316ad0e9923ad3
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-11-18 16:45:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ while count < 1:
 	returned_bin =  subprocess.check_output(['sudo', 'python', '/home/pi/raspberry/get_temp.py'])
 	returned_bin = os.linesep.join([s for s in returned_bin.splitlines() if s])
 	temp = Decimal(returned_bin)
+	print type(temp)
 	print temp
 	if temp > 23:
 		print 'bigger'
