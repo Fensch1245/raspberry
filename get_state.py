129a5f86cfe2e26f0beea42a9bf40289998f140c
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-11-18 17:10:22 +0100

    Update get_state.py

--- get_state.py
+++ get_state.py
@@ -1,21 +1,7 @@
 import sys
 import ConfigParser
 import os
-# from lxml import html
-# import requests
 
-# page = requests.get('http://192.168.2.102/ay')
-# tree = html.fromstring(page.content)
-
-
-# print(tree.text())
-
-
-#import requests
-
-#r = requests.get('http://192.168.2.102/cm?cmnd=Power')
-#print(r.text)
- 
 import urllib3
 http = urllib3.PoolManager()
 r = http.request('GET', 'http://192.168.2.123/cm?cmnd=Power')
