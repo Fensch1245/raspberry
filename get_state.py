6cdb6c3f2c182dd99b16c53f889e396eb6783936
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-11-18 17:06:52 +0100

    Update get_state.py

--- get_state.py
+++ get_state.py
@@ -21,12 +21,13 @@ import urllib3
 http = urllib3.PoolManager()
 r = http.request('GET', 'http://192.168.2.123/cm?cmnd=Power')
 
-if 'OFF' in r.data:
-	status = 0
-else:
-	status = 1
+print r
+# if 'OFF' in r.data:
+	# status = 0
+# else:
+	# status = 1
 
-print status
+# print status
 
 	#http://192.168.2.102/cm?cmnd=status
 	
