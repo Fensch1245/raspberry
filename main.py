7ba67c1fe22e1f5bddf5a81231fd3ae112cee1ea
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-11-18 16:42:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,12 +15,10 @@ while count < 1:
 	returned_bin =  subprocess.check_output(['sudo', 'python', '/home/pi/raspberry/get_temp.py'])
 	returned_bin = os.linesep.join([s for s in returned_bin.splitlines() if s])
 	float(returned_bin)
-	print returned_bin
-	print type(returned_bin)
-	# if returned_bin > 23
-		# print 'bigger'
-	# else
-		# print 'smaller'
+	if returned_bin > 23
+		print 'bigger'
+	else
+		print 'smaller'
 	
 	time.sleep(2)
 #	returned_bin =  subprocess.check_output('/home/pi/raspberry/get_temp.py'.split())
