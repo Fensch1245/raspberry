d19bec9bd57ebc0d59e8a015ffe534e2c0d6db7e
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-11-18 16:26:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ while count < 1:
 #	import get_temp
 #	val = get_temp.ret_val()
 #	print val
-	returned_bin =  subprocess.check_output('sudo python get_temp.py')
+	returned_bin =  subprocess.run('sudo python /home/pi/raspberry/get_temp.py')
 	time.sleep(2)
 #	returned_bin =  subprocess.check_output('/home/pi/raspberry/get_temp.py'.split())
 	
