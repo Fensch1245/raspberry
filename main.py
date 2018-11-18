5e0fc5a5fb1848c130163853fca942b6d260f6bf
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-11-18 17:13:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,17 @@ from decimal import Decimal
 
 count = 0
 while count < 1:
+# Aktuelle Temperatur des Raumes auslesen
 	returned_temp =  subprocess.check_output(['sudo', 'python', '/home/pi/raspberry/get_temp.py'])
 	returned_temp = os.linesep.join([s for s in returned_temp.splitlines() if s])
 	temp = Decimal(returned_temp) #string in decimal verwandeln
-	
+
+# Eingestellte Temperatur aus Config auslesen
 	returned_settemp =  subprocess.check_output(['sudo', 'python', '/home/pi/raspberry/get_config.py'])
 	returned_settemp = os.linesep.join([s for s in returned_settemp.splitlines() if s])
 	settemp = Decimal(returned_settemp) #string in decimal verwandeln 
-	
+
+# Status der Heizung (Sonoff) auslesen	
 	returned_state =  subprocess.check_output(['sudo', 'python', '/home/pi/raspberry/get_state.py'])
 	returned_state = os.linesep.join([s for s in returned_state.splitlines() if s])
 	state = Decimal(returned_state) #string in decimal verwandeln	
