2e1a12de4e026aab4a5735d7e42e2489d584dd3a
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-11-18 16:52:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,15 +8,17 @@ from decimal import Decimal
 
 count = 0
 while count < 1:
-	returned_bin =  subprocess.check_output(['sudo', 'python', '/home/pi/raspberry/get_temp.py'])
-	returned_bin = os.linesep.join([s for s in returned_bin.splitlines() if s])
-	temp = Decimal(returned_bin) #string in decimal verwandeln
-	print 'Aktuelle Temperatur', temp
+	returned_temp =  subprocess.check_output(['sudo', 'python', '/home/pi/raspberry/get_temp.py'])
+	returned_temp = os.linesep.join([s for s in returned_temp.splitlines() if s])
+	temp = Decimal(returned_temp) #string in decimal verwandeln
+	
+	
+	print 'Aktuelle Temperatur:', temp
 	
 	if temp > 23:
-		print 'bigger'
+		
 	else:
-		print 'smaller'
+		
 	
 	time.sleep(2)
 	
