ce4af7a6b946530e4a7b4f61d537cdb61bf812d6
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-11-24 16:21:11 +0100

    Update log.php

--- web/log.php
+++ web/log.php
@@ -1,9 +1,8 @@
- <?php
-echo nl2br( file_get_contents('/home/pi/raspberry/log.log') );
- ?>
-
 <form action="http://192.168.2.200">
 	</br>
     <input type="submit" value="Hauptseite" />
 </form>
 		
+ <?php
+echo nl2br( file_get_contents('/home/pi/raspberry/log.log') );
+ ?>
