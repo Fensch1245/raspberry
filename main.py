e7b5517a444482d1030a104394769e418c15f88f
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-11-24 15:08:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import os
 import logging
 from decimal import Decimal
 from datetime import datetime
+import urllib.request
 
 logging.basicConfig(filename='log.log',level=logging.DEBUG,format='%(asctime)s %(message)s')
 
@@ -27,16 +28,16 @@ while count < 1:
 	returned_activateheater = os.linesep.join([s for s in returned_activateheater.splitlines() if s])
 	activate_heater = Decimal(returned_activateheater) #string in decimal verwandeln 
 
+	heizungssteuerung_response = urllib.request.urlopen("http://192.168.2.123").getcode()
+	
 	if activate_heater <> 0:
 # Status der Heizung (Sonoff) auslesen	
 		returned_state =  subprocess.check_output(['sudo', 'python', '/home/pi/raspberry/get_state.py'])
 		returned_state = os.linesep.join([s for s in returned_state.splitlines() if s])
 		state = Decimal(returned_state) #string in decimal verwandeln	
 		print 'Status der Heizung:', state
-	else:
-		state = 1
 		
-		
+	print 'Heizungssteuerung antwort: ', heizungssteuerung_response	
 	print 'Aktuelle Temperatur:', temp
 	print 'Eingestellte Temperatur:', settemp	
 	print 'Soll die Heizung gesteuert werden?', activate_heater
@@ -52,10 +53,7 @@ while count < 1:
 				print 'Heizung deaktiviert um :', datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 				logging.info('Heizung deaktiviert')
 				os.system("sudo python /home/pi/raspberry/toggle_state.py")
-	else:
-		if state == 1:
-			os.system("sudo python /home/pi/raspberry/toggle_state.py")
-			state = 0
+
 			
 	time.sleep(5)
 	
