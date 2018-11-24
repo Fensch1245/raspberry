9a49a0e83e7243c7466f5f17869269443048ab2e
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-11-24 16:28:16 +0100

    Update log.php

--- web/log.php
+++ web/log.php
@@ -1,10 +1,8 @@
-<?php
- if(isset($_POST['dellog']))
-        {
-shell_exec('truncate -s 0 /home/pi/raspberry/log.log');
-}
-?>
 
+<form>
+	</br>
+	<button name = "dellog" type="submit">Logdatei leeren</button>
+</form>
 <form action="http://192.168.2.200">
 	</br>
     <input type="submit" value="Hauptseite" />
@@ -12,5 +10,10 @@ shell_exec('truncate -s 0 /home/pi/raspberry/log.log');
 </form>
 		
  <?php
+  if(isset($_POST['dellog']))
+        {
+shell_exec('truncate -s 0 /home/pi/raspberry/log.log');
+}
+
 echo nl2br( file_get_contents('/home/pi/raspberry/log.log') );
  ?>
