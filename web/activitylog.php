df4fbb6436414fcb967de32e975db9f34351842b
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-11-24 17:44:18 +0100

    2

--- web/activitylog.php
+++ web/activitylog.php
@@ -1,13 +1,11 @@
-
-<form method="post">
-	<button name ="dellog" type="submit">Logdatei leeren</button>
-</form>
-
 <form action="http://192.168.2.200">
     <input type="submit" value="Hauptseite" />
 </form>
 <form action="http://192.168.2.200/log.php">
     <input type="submit" value="Logverwaltung" />
+</form>
+<form method="post">
+	<button name ="dellog" type="submit">Logdatei leeren</button>
 </form>
 
  <?php
--- web/templog.php
+++ web/templog.php
@@ -1,16 +1,14 @@
-
-<form method="post">
-	<button name ="dellog" type="submit">Logdatei leeren</button>
-</form>
-
 <form action="http://192.168.2.200">
     <input type="submit" value="Hauptseite" />
 </form>
 <form action="http://192.168.2.200/log.php">
     <input type="submit" value="Logverwaltung" />
 </form>
-	
- <?php
+<form method="post">
+	<button name ="dellog" type="submit">Logdatei leeren</button>
+</form>
+
+<?php
    if(isset($_POST['dellog']))
         {
 exec('sudo truncate -s 0 /home/pi/raspberry/temp.log');
