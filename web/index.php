44e7856a7e725556da8aa5fb648bdedeb1976cfa
Fensch1245 <44620374+Fensch1245@users.noreply.github.com>
2018-11-24 14:55:36 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -11,21 +11,25 @@ if(isset($_POST['update']))
         {
 $temp = $_POST['idtest'];
 shell_exec('sudo python /home/pi/raspberry/ch_config.py DEFAULT set_temp '.escapeshellarg($temp));
+echo "<meta http-equiv='refresh' content='0'>";
+}
+if(isset($_POST['update1']))
+        {
 $heater = $_POST['heater'];
 shell_exec('sudo python /home/pi/raspberry/ch_config.py DEFAULT set_temp '.escapeshellarg($heater));
 echo "<meta http-equiv='refresh' content='0'>";
 }
-
  ?>
 
 <html>
 <form name="update" method="post" >
 	<b> Eingestellte Temperatur: </b> </br>
 	<input type="text" name="idtest" value="<?php echo exec('sudo python /home/pi/raspberry/get_config.py'); ?>" />
-    <input type="text" name="heater" value="<?php echo exec('sudo python /home/pi/raspberry/get_config_heater.py'); ?>" />
 	<button name = "update" type="submit">Speichern</button>
 	
-	
+	<b> Heizungsstauerung aktiv: </b> </br>
+	<input type="text" name="heater" value="<?php echo exec('sudo python /home/pi/raspberry/get_config_heater.py'); ?>" />
+	<button name = "update1" type="submit">Speichern</button>
 </form>
 </html>
 		 
